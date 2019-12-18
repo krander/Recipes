fbd365f95283a923fdc8f837c215683a49c35a53
Katie Anderson <31149472+krander@users.noreply.github.com>
2019-12-18 09:42:34 -0500

    Update randomLinks.js

--- js/randomLinks.js
+++ js/randomLinks.js
@@ -2,7 +2,7 @@
 var randomlinks=new Array()
 randomlinks[0]="https://krander.github.io/Recipes/recipes/BlackBeanAndCornSalad.html"
 randomlinks[1]="https://krander.github.io/Recipes/recipes/CrispySheetPanGnocchiAndVeggies.html"
-randomlinks[2]="https://krander.github.io/Recipes/recipes/MapleBalsamicBrusselsSprouts.html"
+randomlinks[2]="https://krander.github.io/Recipes/recipes/sidedishes/MapleBalsamicBrusselsSprouts.html"
 randomlinks[3]="https://krander.github.io/Recipes/recipes/OnePanMexicanQuinoa.html" 
 randomlinks[4]="https://krander.github.io/Recipes/recipes/PastaPrimavera.html"  
 randomlinks[5]="https://krander.github.io/Recipes/recipes/PineappleFriedRice.html"
